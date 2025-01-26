c67abbb3bbe08d69f10ccd7412902b30cd7fb0e8
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-01-26 12:43:31 +0530

    Correct Password Solved!

---
+++ CM Sheet/C++ Basics/Correct Password.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    while (N != 1999)
+    {
+        cin >> N;
+        if (N != 1999)
+            cout << "Wrong" << endl;
+        else
+            cout << "Correct" << endl;
+    }
+
+    return 0;
+}
