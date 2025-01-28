5e73f366ec737a6e5b5820ffe62ebc89b146f88f
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-01-28 12:44:13 +0530

    Search in a Matrix Added!

---
+++ CM Sheet/C++ Basics/Search in a Matrix.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int target;
+    cin >> target;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == target)
+            {
+                cout << "will not take number" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "will take number" << endl;
+    return 0;
+}
