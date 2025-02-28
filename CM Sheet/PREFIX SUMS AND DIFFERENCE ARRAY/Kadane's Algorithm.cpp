1011d68bd605af7b1284899bce7699d6a5895f2b
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-03-01 00:27:06 +0530

    Kadane's Algorithm Added!

---
+++ CM Sheet/PREFIX SUMS AND DIFFERENCE ARRAY/Kadane's Algorithm.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        // Kadane's Algorithm to Find The Maximum Sum Subarray
+        // Where Positive and Negative elements are there.
+
+        // Extends the existing Subarray
+        // Or
+        // Starts a New Subarray
+
+        // Extends the existing subarray only if
+        //   the subarray sum + current>current
+
+        // Starts New if And Only
+        // The subarray sum+current < current
+
+        int subarraysum, maxsum;
+        subarraysum = maxsum = nums[0];
+        for (int current = 1; current < nums.size(); current++)
+        {
+            subarraysum = max(nums[current], subarraysum + nums[current]);
+            maxsum = max(maxsum, subarraysum);
+        }
+        return maxsum;
+    }
+};
