28b73cdaa4a760b46113d6000a4acd0eb0138f0f
CoderMayukh12 <mayukhbhowmik.cse2022@nsec.ac.in>
2025-03-16 22:14:29 +0530

    Priority Queue Uploaded!

---
+++ CM Sheet/C++ STL/Priority_queue.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mod 1000000007
+
+int32_t main()
+{
+    fast
+        priority_queue<int>
+            s;
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        string operation;
+        cin >> operation;
+        if (operation == "push")
+        {
+            int x;
+            cin >> x;
+            s.push(x);
+        }
+        else if (operation == "pop")
+        {
+            s.pop();
+        }
+        else
+        {
+            cout << s.top() << endl;
+        }
+    }
+    return 0;
+}
